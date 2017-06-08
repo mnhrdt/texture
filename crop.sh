8bcd8f24a26ff1c2e1f321b25781d7c9263aced9
mariedautume <mariedautume@ns321058.ip-37-187-153.eu>
2017-06-08 19:27:33 +0200

    modifications pour que crop.sh marche aussi sur boucantrin

--- crop.sh
+++ crop.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
 # auteur : mariedautume
 
+# exemple input bash crop.sh /home/coco/src/s2p-iarpa/input/ essai/ data/small_Challenge1_Lidar.tif
+set -e
 img_dir=$1
 exp_dir=$2
+lidar=$3
 
-#mkdir $exp_dir/
-#mkdir $exp_dir/data/
-#mkdir $exp_dir/data/minMax/
-#mkdir $exp_dir/data/cropped/
-#
-#for i in {01..47}; do
-#    bin/get_corners data/Challenge1_Lidar.tif -21 $img_dir/img_$i.rpc > $exp_dir/data/minMax/minMaxWH_img_$i.txt
-#    read xmin ymin width height < $exp_dir/data/minMax/minMaxWH_img_$i.txt
-#    gdal_translate -ot uint16 -srcwin $xmin $ymin $width $height $img_dir/img_$i.ntf $exp_dir/data/cropped/cropped_img_$i.tif
-#done
+IDX="01 02 03 04 05"
 
-#mkdir $exp_dir/images/
-#
-#for i in $exp_dir/cropped/*tif; do
-#    qeasy 100 1100 $i ${i/tif/png}
-#    convert ${i/tif/png} ${i/tif/jpg}
-#done
-#mv $exp_dir/cropped/*jpg $exp_dir/cropped_jpg/
-#
+mkdir -p $exp_dir/
+mkdir -p $exp_dir/data/
+mkdir -p $exp_dir/data/minMax/
+mkdir -p $exp_dir/data/cropped/
 
-#mkdir $exp_dir/data/proj
-#
-#for i in {01..47}; do 
-#    read xmin ymin width height < $exp_dir/data/minMax/minMaxWH_img_$i.txt
-#    echo "bin/get_P_of_crop data/Challenge1_Lidar.tif -21 $exp_dir/data/rpc/img_$i.rpc $xmin $ymin essai.tif > $exp_dir/data/proj/P_img_$i.txt"
-#    bin/get_P_of_crop data/Challenge1_Lidar.tif -21 $exp_dir/data/rpc/img_$i.rpc $xmin $ymin essai.tif > $exp_dir/data/proj/P_img_$i.txt
-#done
-#
-#for i in {01..47}; do 
-#    python src/decomp_affine.py essai_iarpa/data/proj/P_img_$i.txt
-#done
-mkdir $exp_dir/data/matches
-for i in {01..47}; do
+# crée à partir des grandes images .ntf des petites images tif contenant la zone du lidar 
+for i in $IDX; do
+    bin/get_corners $lidar -21 $img_dir/img_$i.rpc > $exp_dir/data/minMax/minMaxWH_img_$i.txt
+    read xmin ymin width height < $exp_dir/data/minMax/minMaxWH_img_$i.txt
+    gdal_translate -ot uint16 -srcwin $xmin $ymin $width $height $img_dir/img_$i.ntf $exp_dir/data/cropped/cropped_img_$i.tif
+done
+
+mkdir -p $exp_dir/data/images/
+
+# quantifie les petites images tif et les convertit en jpg
+for i in $exp_dir/data/cropped/*tif; do
+    qeasy 100 1100 $i ${i/tif/png}
+    convert ${i/tif/png} ${i/tif/jpg}
+done
+mv $exp_dir/data/cropped/*jpg $exp_dir/data/images/
+
+
+mkdir -p $exp_dir/data/proj
+
+# construit la matrice de projection pour chaque image
+for i in $IDX; do 
+    read xmin ymin width height < $exp_dir/data/minMax/minMaxWH_img_$i.txt
+    bin/get_P_of_crop $lidar -21 $img_dir/img_$i.rpc $xmin $ymin essai.tif > $exp_dir/data/proj/P_img_$i.txt
+done
+
+# calcule K, R et C à partir de P
+for i in $IDX; do 
+    python src/decomp_affine.py $exp_dir/data/proj/P_img_$i.txt
+done
+
+mkdir -p $exp_dir/data/matches
+
+# localise le lidar sur l'image puis reprojette sur lidar (donne les images avec grille)
+for i in $IDX; do
     echo "bin/colorize_with_shadows data/Challenge1_Lidar.tif $exp_dir/data/cropped/cropped_img_$i.tif $exp_dir/data/proj/P_img_$i.txt $exp_dir/data/matches/matches_lidar_img_$i.tif"
     bin/colorize_with_shadows data/Challenge1_Lidar.tif $exp_dir/data/cropped/cropped_img_$i.tif $exp_dir/data/proj/P_img_$i.txt $exp_dir/data/matches/matches_lidar_img_$i.tif
 done
