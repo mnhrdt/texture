7ef0d626249a03439bfade44beaabdaa42d57465
mariedautume <marie@pclnrs212.biomedicale.univ-paris5.fr>
2017-11-23 12:38:09 +0100

    ajout du script que j'utilise en ce moment

---
+++ scripts/scaledim2colouredcurve.sh
@@ -0,0 +1,39 @@
+#!/binbash
+# auteur : mariedautume
+
+# exemple input bash im2colouredurve.sh 01
+set -e
+set -x
+im=$1
+thresh=$2
+
+read x y s z < data/ncc_shift/ncc_shift_$im.txt
+
+bin/zbuffer \
+    data/mcdsm/outdir_$im*/mcdsm.tif \
+    -21 \
+    exp/data/crop/crop_img_$im.tif  \
+    data/PAN/img_$im.rpc \
+    exp/output/xywh/xywh_$im.txt \
+    exp/output/mesh_curve_scaled_remeshed_02.off \
+    exp/soutput/matches/matches_$im.tif \
+    -ox $x -oy $y -oz $z -xmin 1505 -ymin 891 --res 0.45
+
+bin/colormultiple_mesh \
+    exp/output/mesh_curve_scaled_remeshed_02.off \
+    exp/output/xywh/xywh_$im.txt \
+    data/PAN/img_$im.ntf \
+    data/PAN/img_$im.rpc \
+    data/MSI/img_$im.ntf \
+    data/MSI/img_$im.xml \
+    exp/soutput/matches/matches_$im.tif  \
+    exp/soutput/vc_$im.tif
+
+qauto -i -f -p 0.5 exp/soutput/vc_$im.tif exp/soutput/vc_$im.tiff
+# qauto -i -f exp/soutput/vc_$im.tif exp/soutput/vc_$im.tiff
+
+bin/write_coloured_ply \
+    exp/output/mesh_curve_scaled_remeshed_02.off \
+    exp/soutput/vc_$im.tiff \
+    exp/soutput/ccoloured_$im\_02.ply 
+
