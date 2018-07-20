5d612f13bcada95de0c4879fe159ebbedaf29543
mariedautume <anonymous@overleaf.com>
2018-07-20 17:59:12 +0200

    ajout de commentaires

--- scripts/entire.sh
+++ scripts/entire.sh
@@ -1,7 +1,12 @@
-#!/binbash
+#!/bin/bash
 # auteur : mariedautume
 
-# exemple input bash all_new.sh 01
+# exemple d'utilisation à partir de ../
+# bash entire.sh config.sh contrast_method fusion_method output_dir
+# exemple de config.sh dans scripts/config_try.sh
+# contrast_method = mean_var pour l'instant
+# fusion_method   = frontal_{vertices, edges}, scalar_{vertices, edges}
+
 set -e
 set -x
 config=$1
@@ -13,11 +18,13 @@ output=$4
 
 dir=`mktemp -d`
 
+# create all sub folders
 mkdir -p $dir/mesh
 mkdir -p $dir/fused
 mkdir -p $dir/ncc_shift
 mkdir -p $output/mesh
 
+# if no mesh provided in config.sh, create mesh from reference dsm
 if [ -z "$MESH" ]
 then
 
@@ -37,18 +44,22 @@ then
     MESH=$output/mesh/refined_mesh.off
 fi
 
+############ TO DO #################
+# if mesh provided but no reference dsm create it from mesh
+###################################
+
+# get edges list from mesh
 bin/triproc off2edges \
     $MESH \
     $dir/edges.txt
 
-echo $INPUTS
-
-cat $INPUTS
-
+# loop over all images to get shadow and colour information for each vertex 
+# from one image
 cat $INPUTS | xargs -L1 ./scripts/one_image.sh $dir $config
 
 
 ############# FUSION #############
+# all matlab files are supposed to be in a subfolder called scripts
 folder=`pwd`
 
 matlab -nodesktop -nojvm -nosplash -r\
@@ -56,11 +67,18 @@ matlab -nodesktop -nojvm -nosplash -r\
 
 ######## QUANTIFICATION #########
 
+######## TO DO #################
+# trouver une meilleure méthode
+################################
+
 qauto $dir/fused/fused_$c$f.tif $dir/fused/fused_$c\_$f.tiff
 
+# création du mesh final
 bin/write_coloured_ply \
     $MESH \
     $dir/fused/fused_$c\_$f.tiff \
     $output/mesh/fused_mesh_$c\_$f.ply
      
+
+# suppression des dossiers temporaires
 #rm -r $dir
