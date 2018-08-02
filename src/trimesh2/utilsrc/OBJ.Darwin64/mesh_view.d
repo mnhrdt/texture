b6544645002376dbce081ccc0a960da89faa7640
mariedautume <anonymous@overleaf.com>
2018-08-02 22:17:11 +0200

    [3rd party] for mesh smoothing there is also a pretty good viewer

---
+++ src/trimesh2/utilsrc/OBJ.Darwin64/mesh_view.d
@@ -0,0 +1,45 @@
+OBJ.Darwin64/mesh_view.o: mesh_view.cc ../include/TriMesh.h \
+ ../include/Vec.h ../include/mathutil.h ../include/mathcompat.h \
+ ../include/Box.h ../include/strutil.h ../include/Color.h \
+ ../include/TriMesh_algo.h ../include/TriMesh.h ../include/XForm.h \
+ ../include/lineqn.h ../include/XForm.h ../include/GLCamera.h \
+ ../include/timestamp.h ../include/GLManager.h ../include/ICP.h \
+ ../include/KDtree.h ../include/strutil.h shaders.inc
+
+../include/TriMesh.h:
+
+../include/Vec.h:
+
+../include/mathutil.h:
+
+../include/mathcompat.h:
+
+../include/Box.h:
+
+../include/strutil.h:
+
+../include/Color.h:
+
+../include/TriMesh_algo.h:
+
+../include/TriMesh.h:
+
+../include/XForm.h:
+
+../include/lineqn.h:
+
+../include/XForm.h:
+
+../include/GLCamera.h:
+
+../include/timestamp.h:
+
+../include/GLManager.h:
+
+../include/ICP.h:
+
+../include/KDtree.h:
+
+../include/strutil.h:
+
+shaders.inc:
